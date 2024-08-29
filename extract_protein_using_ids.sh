d552d59b0534e7c904711766e0e0f83ed28aa074
Pooran Dewari <67807603+Pooran-Dewari@users.noreply.github.com>
2024-08-29 23:07:55 +0100

    Update extract_protein_using_ids.sh

--- extract_protein_using_ids.sh
+++ extract_protein_using_ids.sh
@@ -1,3 +1,17 @@
+# requires ids.txt file that contains gene IDs
+# cat ids.txt
+#G8077
+#G8546
+#G11423
+#G4113
+
+# requires a multi-fasta file (translate_agat.fa) that contains all the protein sequences for the entire transcriptome
+# this mutli-fasta protein file can be created using agat perl script that needs genome.fa and genome.gff3 files, see command below
+# agat_sp_extract_sequences.pl -g Crassostrea_gigas.cgigas_uk_roslin_v1.58.chr.gff3 -f Crassostrea_gigas_uk_roslin_v1.dna_sm.primary_assembly.fa -p -o translate_agat.fa
+
+
+# min_len is set to 100, idea is to filter out smaller isoforms to save time
+
 awk -v ids_file="ids.txt" -v min_len=100 '
 BEGIN {
     # Read IDs into an array, appending space to each
